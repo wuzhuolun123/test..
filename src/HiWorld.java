2208b9414e12bb057615f0f8471562e2669d46f2
tmpwoshiwu <tmpwoshiwu@163.com>
2019-07-16 21:47:13 +0800

    test4

--- src/HiWorld.java
+++ src/HiWorld.java
@@ -1,6 +1,6 @@
 public class HiWorld {
     public static void main(String[] args){
-        System.out.println("Hi World!!！！.。...33333。。.");
+        System.out.println("Hi World!!！！.。...33少时诵诗书所所333。。.");
 
     }
 }
